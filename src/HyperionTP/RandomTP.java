b37680aaa0463c92962bef88071d540c9093495c
admin <imhandmade1234@gmail.com>
2018-04-09 18:14:11 -0500

    Don't die in walls, and dont fall in lava

--- src/HyperionTP/RandomTP.java
+++ src/HyperionTP/RandomTP.java
@@ -1,6 +1,7 @@
 package HyperionTP;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,11 +13,12 @@ public class RandomTP implements CommandExecutor {
             Player player = (Player) sender;
 
             int randomX = (int) (Math.random()*2000);
-            int randomY = (int) (Math.random()*256);
             int randomZ = (int) (Math.random()*2000);
+            int randomY = player.getWorld().getHighestBlockYAt(randomX, randomZ);
 
             Location rtp = new Location(player.getWorld(),randomX, randomY, randomZ );
             player.teleport(rtp);
+            rtp.getBlock().setType(Material.DIRT);
             player.sendMessage("You were teleported to X: " + randomX + " Y: " + randomY " Z: " + randomZ);
         }
         return false;
