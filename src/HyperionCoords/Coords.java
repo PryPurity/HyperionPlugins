6a41d3e8fa57d4d84f1d6f6ae153cbb16e949eef
admin <imhandmade1234@gmail.com>
2018-04-11 19:54:03 -0500

    wat

--- src/HyperionCoords/Coords.java
+++ src/HyperionCoords/Coords.java
@@ -9,7 +9,7 @@ public class Coords implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] strings) {
         if (commandLabel.equalsIgnoreCase("coords") || commandLabel.equalsIgnoreCase("coordinates")) {
                 Player player = (Player) sender;
-            if ((!player.hasPermission("hyperion.coords")) && (player.isOp())) {
+            if ((!player.hasPermission("hyperion.cords")) && (player.isOp())) {
 
                 player.sendMessage("X: " + player.getLocation().getBlockX() + ", Y: " + player.getLocation().getBlockY() + ", Z: " + player.getLocation().getBlockZ());
             }
